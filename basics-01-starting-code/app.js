239215b180659b6c22866d94c01358742ded53c5
Matheus Barros <matheusborn@hotmail.com>
2021-10-12 17:14:15 -0300

    Outputting Raw HTML Content with v-html

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,15 +1,16 @@
 const App = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: '<h1>Master Vue and build amazing apps!</h1>',
       vueLink: 'https://vuejs.org/'
     }
   },
   methods: {
     outputGoal() {
       const randomNumber = Math.random()
-      if(randomNumber < 0.5) return 'Learn Vue!'
-      return 'Master Vue!'
+      if(randomNumber < 0.5) return this.courseGoalA
+      return this.courseGoalB
     }
   }
 });
