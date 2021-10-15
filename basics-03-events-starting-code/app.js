256e1e1d49a6eba46a1587961eae3339becb2ecb
Matheus Barros <matheusborn@hotmail.com>
2021-10-15 10:12:40 -0300

    Using Native Event Object

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,11 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     }
   },
   methods: {
     add(number) {
       this.counter += number
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName
     }
   }
 });
