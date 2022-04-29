f84bc1e861cdfa54b396347c4956cc6872db9dba
Matheus Barros <matheusborn@hotmail.com>
2022-04-29 15:39:16 -0300

    Validating props

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,6 +1,7 @@
 <template>
   <li>
-    <h2>{{name}}</h2>
+    <h2>{{name}} {{isFavorite === '1' ? '(Favorite)' : ''}}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
     <ul v-if="detailsAreVisible">
       <li>
@@ -17,7 +18,29 @@
 
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress"],
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function(value) {
+        return ["0", "1"].includes(value);
+      }
+    }
+  },
   data() {
     return {
       detailsAreVisible: false
