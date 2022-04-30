db70153b18046d6c4a1525bdcf019a1e11df1969
Matheus Barros <matheusborn@hotmail.com>
2022-04-30 08:35:20 -0300

    Adding Components & Conenecting Them

---
+++ vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="Name">Name</label>
+      <input type="text" v-model="name" />
+    </div>
+    <div>
+      <label for="Phone">Phone</label>
+      <input type="text" v-model="phone" />
+    </div>
+    <div>
+      <label for="Email">E-Mail</label>
+      <input type="text" v-model="email" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewFriend",
+  emits: ["add-contact"],
+  data() {
+    return {
+      name: "",
+      phone: "",
+      email: ""
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("add-contact", {
+        name: this.name,
+        phone: this.phone,
+        email: this.email
+      });
+    }
+  }
+};
+</script>
