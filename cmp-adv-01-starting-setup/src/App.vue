cc66fff54d03726849fef883db52848693ea09e6
Matheus Barros <matheusborn@hotmail.com>
2022-05-01 15:44:14 -0300

    Dynamic Components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,46 +1,45 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps.anotherProp }}</p>
-      </template>
-    </course-goals>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps.anotherProp }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>-->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
-import BadgeList from "@/components/BadgeList";
+// import BadgeList from "@/components/BadgeList";
 import TheHeader from "@/components/TheHeader";
-import UserInfo from "@/components/UserInfo";
-import CourseGoals from "@/components/CourseGoals";
+// import UserInfo from "@/components/UserInfo";
+// import CourseGoals from "@/components/CourseGoals";
+import ActiveGoals from "@/components/ActiveGoals";
+import ManageGoals from "@/components/ManageGoals";
 
 export default {
   components: {
-    BadgeList,
-    CourseGoals,
+    // BadgeList,
+    // CourseGoals,
     TheHeader,
-    UserInfo
+    // UserInfo,
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin"
       }
     };
+  },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    }
   }
 };
 </script>
