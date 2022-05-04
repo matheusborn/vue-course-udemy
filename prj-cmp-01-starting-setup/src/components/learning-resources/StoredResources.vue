0c3f479cdd2da83f525aecd8852e01673d6f5746
Matheus Barros <matheusborn@hotmail.com>
2022-05-04 08:23:14 -0300

    Syling and More Components

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in resources"
+      :key="resource.id"
+      :description="resource.description"
+      :link="resource.link"
+      :title="resource.title"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from '@/components/learning-resources/LearningResource';
+
+export default {
+  components: {
+    LearningResource
+  },
+  props: {
+    resources: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
