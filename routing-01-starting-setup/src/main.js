0f61c1c23bf9888f7d49f2da259df1fbf707909e
Matheus Barros <matheusborn@hotmail.com>
2022-05-24 14:29:05 -0300

    routing setup, registering & rendering routes, navigation with router-link, styling active links

---
+++ routing-01-starting-setup/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams',
+      component: () => import('./components/teams/TeamsList.vue'),
+    },
+    {
+      path: '/users',
+      component: () => import('./components/users/UsersList.vue'),
+    },
+  ],
+  linkActiveClass: 'active',
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
