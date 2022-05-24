966b1a90e934e60c060fac6380cafb9c1b98a55a
Matheus Barros <matheusborn@hotmail.com>
2022-05-23 21:02:35 -0300

    Sending a post http request

--- http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -43,10 +43,10 @@ export default {
     return {
       enteredName: '',
       chosenRating: null,
-      invalidInput: false,
+      invalidInput: false
     };
   },
-  emits: ['survey-submit'],
+  // emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,15 +55,29 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
+
+      fetch(
+        'https://vue-http-demo-5699c-default-rtdb.firebaseio.com/surveys.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            userName: this.enteredName,
+            rating: this.chosenRating
+          })
+        }
+      );
 
       this.enteredName = '';
       this.chosenRating = null;
-    },
-  },
+    }
+  }
 };
 </script>
 
