2c770b01df813c16ca2a72135f335134e1c15771
Matheus Barros <matheusborn@hotmail.com>
2022-06-03 13:21:11 -0300

    Introducing Navigation Guards

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -37,12 +37,22 @@ const router = createRouter({
   ],
   linkActiveClass: 'active',
   scrollBehavior(to, from, savedPosition) {
-    console.log(to, from, savedPosition);
+    // console.log(to, from, savedPosition);
     if (savedPosition) {
       return savedPosition;
     }
     return { left: 0, top: 0 };
   },
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('Global beforeEach', to, from);
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  next();
 });
 
 const app = createApp(App);
