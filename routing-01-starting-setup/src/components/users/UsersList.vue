c7250b9ccdb865bb8a9eeec776590156cfb120d6
Matheus Barros <matheusborn@hotmail.com>
2022-06-03 15:51:28 -0300

    Beyond Entering: Route Leave Guards

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -25,6 +25,14 @@ export default {
     // ...
     console.log('UserList beforeRouteEnter', to, from);
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    // ...
+    console.log('UserList beforeRouteLeave', to, from);
+    next(confirm('Are you sure you want to leave? You got unsaved changes'));
+  },
+  unmounted() {
+    console.log('UserList unmounted');
   }
 };
 </script>
