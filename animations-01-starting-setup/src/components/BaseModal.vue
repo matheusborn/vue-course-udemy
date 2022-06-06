2ca132110966f811bfec2c6ba6b3c64fc3abbeac
Matheus Barros <matheusborn@hotmail.com>
2022-06-06 13:13:00 -0300

    Why 'just css' not enough

--- animations-01-starting-setup/src/components/BaseModal.vue
+++ animations-01-starting-setup/src/components/BaseModal.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div>
+    <div class="backdrop" @click="$emit('close')"></div>
+    <dialog open>
+      <slot></slot>
+    </dialog>
+  </div>
 </template>
 
 <script>
 export default {
-  emits: ['close'],
+  emits: ['close']
 };
 </script>
 
@@ -34,5 +36,18 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
+  animation: modal 0.3s ease-out forwards;
+}
+
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
 }
 </style>
