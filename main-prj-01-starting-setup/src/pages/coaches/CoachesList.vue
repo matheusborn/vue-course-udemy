a699b41a741ad35707d5e64a5aea46f0080c7c7d
Matheus Barros <matheusborn@hotmail.com>
2022-07-22 18:45:17 -0300

    Adding a Couple of Base Components

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -3,16 +3,18 @@
     FILTER
   </section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item v-for="coach in filteredCoaches" :last-name="coach.lastName" :first-name="coach.firstName"
-        :key="coach.id" :id="coach.id" :rate="coach.hourlyRate" :areas="coach.areas">
-      </coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item v-for="coach in filteredCoaches" :last-name="coach.lastName" :first-name="coach.firstName"
+          :key="coach.id" :id="coach.id" :rate="coach.hourlyRate" :areas="coach.areas">
+        </coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
